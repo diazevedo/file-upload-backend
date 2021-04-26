f17805dadac6806c58da38adcf1c69eefce4976c
Diego Azevedo <dapptn@hotmail.com>
2021-04-26 17:16:23 +1000

    added new index with no sensitive data

---
+++ index.js
@@ -0,0 +1,90 @@
+const express = require("express");
+const crypto = require("crypto");
+const path = require("path");
+const mongoose = require("mongoose");
+const multer = require("multer");
+const GridFsStorage = require("multer-gridfs-storage");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+
+// connection
+const conn = mongoose.createConnection(process.env.MONGO_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+// init gfs
+let gfs;
+conn.once("open", () => {
+  gfs = new mongoose.mongo.GridFSBucket(conn.db, {
+    bucketName: "uploads",
+  });
+});
+
+// Storage
+const storage = new GridFsStorage({
+  url: process.env.MONGO_URL,
+  file: (req, file) => {
+    return new Promise((resolve, reject) => {
+      crypto.randomBytes(16, (err, buf) => {
+        if (err) {
+          return reject(err);
+        }
+
+        const filename = buf.toString("hex") + path.extname(file.originalname);
+        const fileInfo = {
+          filename: filename,
+          bucketName: "uploads",
+        };
+        resolve(fileInfo);
+      });
+    });
+  },
+});
+
+const upload = multer({
+  storage,
+});
+
+app.get("/", (req, res) => {
+  gfs.find().toArray((err, files) => {
+    // check if files
+    if (!files || files.length === 0) {
+      return res.status(404).json({
+        err: "no files exist",
+      });
+    }
+
+    return res.json(files);
+  });
+});
+
+app.get("/files/:filename", async (req, res) => {
+  gfs
+    .find({
+      filename: req.params.filename,
+    })
+    .toArray((err, files) => {
+      if (!files || files.length === 0) {
+        return res.status(404).json({
+          err: "no files exist",
+        });
+      }
+
+      gfs.openDownloadStreamByName(req.params.filename).pipe(res);
+    });
+});
+
+app.post("/files", upload.single("file"), (req, res) => {
+  res.status(201).json({
+    file: req.file,
+  });
+});
+
+const port = process.env.PORT || 3333;
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
